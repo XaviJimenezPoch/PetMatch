27e4fc1ec059e1f0f3c5c155ae2fc889aec39567
Núria <ruizmunoznuria@gmail.com>
2025-11-12 18:52:39 +0100

    antes de las migraciones

---
+++ mascotas/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Mascota(models.Model):
+    # Tipos de mascota
+    TIPO_MASCOTA = [
+        ('perro', 'Perro'),
+        ('gato', 'Gato'),
+    ]
+    
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=10, choices=TIPO_MASCOTA, default='perro')
+    foto = models.ImageField(upload_to='mascotas/')
+    descripcion = models.TextField()
+    edad = models.PositiveIntegerField(help_text="Edad en años")
+    ubicacion = models.CharField(max_length=100, default="Ciudad")
+    
+    # Relación con el usuario que sube la mascota
+    propietario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.nombre} ({self.tipo})"
+    
+    class Meta:
+        verbose_name = "Mascota"
+        verbose_name_plural = "Mascotas"
