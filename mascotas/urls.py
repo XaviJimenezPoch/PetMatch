54feeedffc3ced0885738bd39446463763ed460f
XaviJimenezPoch <xavijimenezpoch98@gmail.com>
2025-11-13 19:46:48 +0100

    cambios en todo

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'mascotas', MascotaViewSet)
-
 
 urlpatterns = [
     path('subir/', views.subir_mascota, name='subir_mascota'),
     path('listar/', views.listar_mascotas, name='listar_mascotas'),
-     path('api/', include(router.urls)),
+    path('<int:mascota_id>/', views.detalle_mascota, name='detalle_mascota'),
+    path('mis-mascotas/', views.mis_mascotas, name='mis_mascotas'),
+    path('eliminar/<int:mascota_id>/', views.eliminar_mascota, name='eliminar_mascota'),
 ]
